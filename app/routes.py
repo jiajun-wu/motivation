f7be2456d26b01936af423e61e8a3d72391bb447
jiajun-wu <jiajun1009@gmail.com>
2019-11-16 09:36:47 -0800

    added webhook to routes.py

--- app/routes.py
+++ app/routes.py
@@ -10,15 +10,19 @@ def webhook():
     challenge = request.args.get('hub.challenge')
     verify_token = request.args.get('hub.verify_token')
 
-    if (mode === "subscribe" && verify_token === VERIFY_TOKEN) {
-      // Responds with the challenge token from the request
-      console.log("WEBHOOK_VERIFIED");
-      res.status(200).send(challenge);
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
-    }
+    print mode
     print challenge
+    print verify_token
+
+
+    # if (mode === "subscribe" && verify_token === VERIFY_TOKEN) {
+    #   // Responds with the challenge token from the request
+    #   console.log("WEBHOOK_VERIFIED");
+    #   res.status(200).send(challenge);
+    # } else {
+    #   // Responds with '403 Forbidden' if verify tokens do not match
+    #   res.sendStatus(403);
+    # }
     return 'here'
 
 @app.route('/index')
