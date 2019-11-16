7ebade32ad0e3d2ad4d2002492c7d597bd4704a0
jiajun-wu <jiajun1009@gmail.com>
2019-11-16 09:34:37 -0800

    added webhook to routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,24 @@
 from app import app
-from flask import request
+from flask import request,
 
 @app.route('/')
 
 @app.route('/webhook')
 def webhook():
     VERIFY_TOKEN = 'fbsfhachatohon2019'
+    mode = request.args.get('hub.mode')
     challenge = request.args.get('hub.challenge')
-    # print challenge
+    verify_token = request.args.get('hub.verify_token')
+
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      // Responds with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+    print challenge
     return 'here'
 
 @app.route('/index')
