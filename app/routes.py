2f84be4ca50d04f1d9c843be2b1b352ab912f11a
jiajun-wu <jiajun1009@gmail.com>
2019-11-16 08:51:39 -0800

    added webhook to routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from app import app
 
-@app.route('/')
+@app.route('/webhook')
+def webhook():
+    VERIFY_TOKEN = 'fbsfhachatohon2019'
+    return "you here"
+
 @app.route('/index')
 def index():
     return "Hello, World!"
