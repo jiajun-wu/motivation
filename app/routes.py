78296799fba8f5a9e21ade8f5c561b637c10fe69
jiajun-wu <jiajun1009@gmail.com>
2019-11-16 09:22:45 -0800

    added webhook to routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,13 @@
 from app import app
+from flask import request, response
 
 @app.route('/')
-@app.route('/webhook')
+@app.route('/webhook', methods=['GET'])
 def webhook():
     VERIFY_TOKEN = 'fbsfhachatohon2019'
-    return "you here"
+    challenge = request.args.get('hub.challenge')
+    print challenge
+    return challenge
 
 @app.route('/index')
 def index():
