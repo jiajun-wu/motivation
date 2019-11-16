1b4a97704c6a7f06aa24a7041c961f5d1d81ea30
jiajun-wu <jiajun1009@gmail.com>
2019-11-16 15:23:21 -0800

    added payload

--- app/routes.py
+++ app/routes.py
@@ -162,8 +162,8 @@ def switch_payload(payload):
             }
         }
     elif payload == 'cooking':
-        res_text = 'there we go for other options'
-        payload_json =
+        res_text = 'here we go for other options'
+        payload_json = {
           "text": res_text,
           "quick_replies":[
               {
