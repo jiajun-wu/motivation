080ec2fdaaa99b3d1cd0752208cac52fe27b9d39
jiajun-wu <jiajun1009@gmail.com>
2019-11-16 13:29:11 -0800

    v2

--- app/routes.py
+++ app/routes.py
@@ -93,7 +93,7 @@ def callSendAPI(sender_psid, response):
 
     print(sender_psid)
     print(request_body)
-    print('I am here')
+    print('I am here2')
 
     r = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=EAALcUGICU5cBAL9aAExF6kCqSNzvreSOsmRBYy4tDZA5RaZCrxsZBnVtZB87IRRD2GvoSWVxIaGRg6vTsWBRvZCdyeFOT6jhuJViPyCtuyq88DGf4MHYShisme9wRJv4Bdkn6YwTzZCCUMsovhIjd8RFiG8K7INHPx1J37o4LAMAZDZD",json=request_body)
 
